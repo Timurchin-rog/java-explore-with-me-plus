cb8ea799f96dd4c98a4871ecb0821a6008c96613
Timur Myusov <drakonhftfg@yandex.ru>
2025-06-09 15:50:41 +0700

    Наметил структуру проекта. Код не писал

---
+++ stat-service/src/main/java/ru/practicum/ewm/EWMStat.java
@@ -0,0 +1,12 @@
+package ru.practicum.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWMStat {
+	public static void main(String[] args) {
+		SpringApplication.run(EWMStat.class, args);
+	}
+
+}
