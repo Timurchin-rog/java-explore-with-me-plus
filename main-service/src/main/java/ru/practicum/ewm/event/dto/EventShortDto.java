768ec22b5ce9e9f652aaece6a49393ce728316cc
Timur Myusov <drakonhftfg@yandex.ru>
2025-06-23 22:48:32 +0700

    Реализовал сущности, dto и мапперы

---
+++ main-service/src/main/java/ru/practicum/ewm/event/dto/EventShortDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.ewm.event.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.ewm.category.dto.CategoryDto;
+import ru.practicum.ewm.user.dto.UserShortDto;
+
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EventShortDto {
+    Long id;
+    String annotation;
+    CategoryDto category;
+    Long confirmedRequests;
+    String eventDate;
+    UserShortDto initiator;
+    Boolean paid;
+    String title;
+    Long views;
+}
