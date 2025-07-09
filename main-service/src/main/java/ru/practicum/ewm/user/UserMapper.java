1b4468d3c8c30ee4e1e0e08b0a580709411edb2f
michailb <michailb@kmslh.com>
2025-07-09 10:59:50 +0300

    main_svc
    - Refactor user validation

--- main-service/src/main/java/ru/practicum/ewm/user/UserMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/user/UserMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.user;
 
-import ru.practicum.ewm.exception.ValidationException;
 import ru.practicum.ewm.user.dto.NewUserRequest;
 import ru.practicum.ewm.user.dto.UserDto;
 import ru.practicum.ewm.user.dto.UserShortDto;
@@ -9,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserMapper {
+
     public static UserDto mapToUserDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
@@ -36,28 +36,8 @@ public class UserMapper {
 
     public static User mapFromRequest(NewUserRequest user) {
         return new User(
-                validateEmail(user.getEmail()),
-                validateName(user.getName())
+                user.getEmail(),
+                user.getName()
         );
-    }
-
-    private static String validateEmail(String email) {
-        if (email.length() < 6) {
-            throw new ValidationException("Имейл не может быть меньше 6 символов");
-        } else if (email.length() > 254) {
-            throw new ValidationException("Имейл не может быть больше 254 символов");
-        } else {
-            return email;
-        }
-    }
-
-    private static String validateName(String name) {
-        if (name.length() < 2) {
-            throw new ValidationException("Имя не может быть меньше 2 символов");
-        } else if (name.length() > 250) {
-            throw new ValidationException("Имя не может быть больше 250 символов");
-        } else {
-            return name;
-        }
     }
 }
